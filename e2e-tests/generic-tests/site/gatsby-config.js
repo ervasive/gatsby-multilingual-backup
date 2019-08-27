08b5428e9b6c054acc649a5509ce510bd829a410
Ruslan Vasiliev <ervasive@gmail.com>
2019-08-27 17:53:58 +0300

    Update e2e-generic-tests setup

--- e2e-tests/generic-tests/site/gatsby-config.js
+++ e2e-tests/generic-tests/site/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    'theme',
+    'e2e-generic-test-theme',
     {
       resolve: '@gatsby-plugin-multilingual/translations-loader',
       options: { path: 'translations-priority-auto-one' },
