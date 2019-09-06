1f12e7f905f9730dfbab2914ce9008294fda8389
Ruslan Vasiliev <ervasive@gmail.com>
2019-09-07 01:57:08 +0300

    fix: normalize-path prepend slash

--- packages/gatsby-plugin-multilingual/src/utils/__tests__/normalize-path.test.ts
+++ packages/gatsby-plugin-multilingual/src/utils/__tests__/normalize-path.test.ts
@@ -2,6 +2,7 @@ import normalizePath from '../normalize-path'
 
 describe('normalizePath', (): void => {
   it('should return a normalized path', (): void => {
+    expect(normalizePath('path')).toBe('/path')
     expect(normalizePath('/index')).toBe('/')
     expect(normalizePath('/index/')).toBe('/')
     expect(normalizePath('//index/')).toBe('/')
