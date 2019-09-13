3ab25a8659cb95a98227a3751cc951f4a5010831
Ruslan Vasiliev <ervasive@gmail.com>
2019-09-14 01:51:45 +0300

    chore: tune jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '/src/.*/.*\\.test\\.(js|ts)$',
+  testEnvironment: 'jsdom',
+  testRegex: 'packages/.*/src/.*/.*\\.test\\.(js|tsx?)$',
   testPathIgnorePatterns: ['/node_modules/'],
+  setupFiles: [`<rootDir>/jest/loadershim.js`],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.base.json',
     },
+    __PATH_PREFIX__: ``,
   },
   reporters: [
     'default',
