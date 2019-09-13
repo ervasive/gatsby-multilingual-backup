1ff66ccbf551fea97f3630e162dfc2ece8d3fe62
Ruslan Vasiliev <ervasive@gmail.com>
2019-09-14 01:49:43 +0300

    feat: remove Link, Redirect components from export

--- packages/gatsby-plugin-multilingual/src/index.ts
+++ packages/gatsby-plugin-multilingual/src/index.ts
@@ -1,5 +1,3 @@
 export * from 'react-i18next'
 
 export { default as useMultilingual } from './useMultilingual'
-export { default as Link } from './Link'
-export { default as Redirect } from './Redirect'
