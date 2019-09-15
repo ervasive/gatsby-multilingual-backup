04a7bd4caa656172475e6202f60685b7438667a0
Ruslan Vasiliev <ervasive@gmail.com>
2019-09-16 01:54:48 +0300

    Switch to github actions (#38)
    chore(ci): switch to github actions

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '/src/.*/.*\\.test\\.(js|ts)$',
-  testPathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: 'jsdom',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.base.json',
     },
   },
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      { outputDirectory: 'reports/unit-tests', outputName: 'results.xml' },
-    ],
-  ],
+  collectCoverage: true,
 }
